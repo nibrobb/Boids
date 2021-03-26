5aa5ff69a6f15e2a8eec083c9434a56ee5e6d67b
Robin Kristiansen <1232179+nibrobb@users.noreply.github.com>
2021-03-26 15:23:28 +0100

    Added some comments, made it more clear what is what

--- src/config.py
+++ src/config.py
@@ -1,22 +1,22 @@
+# ----- Screen resolution ----- #
 SCREEN_RES = (1280, 720)
+SCREEN_X = SCREEN_RES[0]
+SCREEN_Y = SCREEN_RES[1]
+
 TITLE = "boids by Robin"
 
 FPS = 60
 
 
-SCREEN_X = SCREEN_RES[0]
-SCREEN_Y = SCREEN_RES[1]
-
-
+# ----------- Colors --------- #
 BG_COLOR = (0, 30, 0)
-
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
+# ------ Boid constants ------ #
+BOID_SHAPE = [(15, 0), (0, 50), (15, 45), (30, 50), (15, 0)]
 BOID_WIDTH = 30
 BOID_HEIGHT = 50
 
 BOID_SPEED = 100
 BOID_ROT_SPEED = 250
-
-BOID_SHAPE = [(15, 0), (0, 50), (15, 45), (30, 50), (15, 0)]
