8722152aa370215661935f03e6e1103e0c078b8d
Robin Kristiansen <1232179+nibrobb@users.noreply.github.com>
2021-04-09 14:49:05 +0200

    Finished commenting boid.py

--- src/boid.py
+++ src/boid.py
@@ -32,17 +32,21 @@ class Boid(pygame.sprite.Sprite):
         move = self.calculate_move()
         self.move(move)
 
+
     def calculate_move(self) -> pygame.Vector2:
         """ Calculates the next movement based on the three rules of boids """
-        neighbors = self.get_neighbors()
+        neighbors = self.get_neighbors()           # Find nearby boids
         move = pygame.Vector2()
 
+        # Define a list of rules for easier invokement
         rules = [self.alignment, self.cohesion, self.separation]
         partial_move : pygame.Vector2 = pygame.Vector2()
         for i in range(len(rules)):
-            partial_move = rules[i](neighbors)
-            if (partial_move != pygame.Vector2(0,0)):
+            partial_move = rules[i](neighbors)     # Call each rule and save result as partial_move
+            if (partial_move != pygame.Vector2(0,0)): # Null-check vector
                 if partial_move.magnitude() > self.game.weights[i]:
+                    # If partial_move is greater than its respective weight
+                    #     normalize it and multiply by its respective weight
                     partial_move.normalize()
                     partial_move *= self.game.weights[i]
                 move += partial_move
@@ -51,16 +55,15 @@ class Boid(pygame.sprite.Sprite):
 
     def move(self, move):
         """ Set the next position and rotate image accordingly """
-        self.vel += move
-        if self.vel.magnitude() > MAX_SPEED:
-            self.vel = self.vel.normalize() * MAX_SPEED
+        self.vel += move                                       # Increment velocity by move
+        if self.vel.magnitude() > MAX_SPEED:                   # Check magnitude vs MAX_SPEED
+            self.vel = self.vel.normalize() * MAX_SPEED        # Set vel to max speed
         self.angle = self.vel.angle_to(self.up_vector) % 360   # Find the angle to draw the boid
         self.pos += self.vel * self.game.delta_time            # Caluculate new position
         self.rect = self.image.get_rect(center=self.pos)       # Get a new rect and set its center to pos
-        self.image = pygame.transform.rotate(self.game.boid_img, self.angle)
+        self.image = pygame.transform.rotate(self.game.boid_img, self.angle) # Rotate img according to angle
         
-        ### Some piece of code to not have the boid fly off into the void (i.e. off screen) ###
-        self.wrap()            # Make the boid appear on the other side of the window if it crosses the edge
+        self.wrap() # Boids "wrap" around to the other side of the window when it hits a window-border
 
     # Rule no. 1
     def alignment(self, neighbors : pygame.sprite.Group) -> pygame.Vector2:
@@ -72,8 +75,8 @@ class Boid(pygame.sprite.Sprite):
 
         alignment_move = pygame.Vector2()
         for neighbor in neighbors:
-            alignment_move += neighbor.vel
-        alignment_move /= len(neighbors)
+            alignment_move += neighbor.vel      # Take sum of neighboring boids' direction/heading
+        alignment_move /= len(neighbors)        # Devide by number of neighbors, taking average
 
         return alignment_move
 
@@ -81,8 +84,6 @@ class Boid(pygame.sprite.Sprite):
     # Rule no. 2
     def cohesion(self, neighbors : pygame.sprite.Group) -> pygame.Vector2:
         """ Steer towards the center of mass of nearby boids """
-        # Makes all boids in a radius stay in the same general direction.
-        # A boid should navigate towards the center of all other neighbors
         
         # Find average position of neighboring boids
         if len(neighbors) == 0:
@@ -92,11 +93,8 @@ class Boid(pygame.sprite.Sprite):
             cohesion_move += neighbor.pos
         cohesion_move /= len(neighbors)
 
-        cohesion_move -= self.pos # Getting the offset from the position boid is right now
+        cohesion_move -= self.pos # Getting the offset from the position boid(self) is right now
         return cohesion_move
-        # Funka ikkje som før, men d kanskje bedre for slutten(?)
-        # ok ok ok, se her, alle funksjonan returnere en ny vektor
-        # så det e ikkje sånn at hvert funksjon gjør nokka med vel
 
     
     # Rule no. 3
@@ -107,27 +105,28 @@ class Boid(pygame.sprite.Sprite):
         if len(neighbors) == 0:
             return pygame.Vector2()
 
-        # En kan tenke seg at vi har flere radiier, en for naboer, og en for "plagsomme naboer"
-        # Dette er metoden som håndterer den sistnevnte
-        # Vi tenker oss en radius inni nabo-radiusen som hvis det forekommer noen boids i skal vi
-        # prøve å få større avstand til
+        # Imagine we have several radii, one for general neighbors, and one for "annoying neighbors"
+        # We want to steer away from the ones that are too close, indicated by the AVOIDANCE_RADIUS
         separation_move = pygame.Vector2()
-        n_avoid = 0
+        n_avoid = 0                 # Variable for storing number of "annoying neighbors"
 
         for neighbor in neighbors:
             if (neighbor.pos.distance_to(self.pos) < AVOIDANCE_RADIUS):
                 n_avoid += 1
                 separation_move += (self.pos - neighbor.pos)
+                # We are adding to separation_move the differance between our(self) position
+                # and the neighboring (too close) boid
+                # This will consequently make separation_move opposite of the average position
+                #   of neighboring boids that are too close
         
         if n_avoid > 0:
             separation_move /= n_avoid
+            # Taking the average
         return separation_move
 
 
-
-
     def get_neighbors(self) -> pygame.sprite.Group:
-        """ Code for finding neighbors """
+        """ Find neighboring boids within a defined radius and return them as a sprite group """
         neighbors = pygame.sprite.Group()
         for boid in self.game.all_boids:
             dist = self.pos.distance_to(boid.pos)
